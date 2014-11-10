5d3327a740b7292fc49a4b89e78c4b686cf118fa
Camilla van Klinken <camillavk@gmail.com>
2014-11-10 17:56:57 +0000

    s£ protocol added

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ActiveRecord::Base
   has_attached_file :image, :styles => {
     :medium => '600x600>',
     :thumb => '300x300>'},
-    :default_url => '/images/:style/missing.png'
+    :default_url => '/images/:style/missing.png', :s3_protocol => :https
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   # validates :image, presence: true
 
